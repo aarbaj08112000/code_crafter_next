b27e1ce093e09e16c260b3a1a4002e478069691a
aarbaj08112000 <162178583+aarbaj08112000@users.noreply.github.com>
2026-06-04 18:53:45 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,10 +2,14 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "srv1493.hstgr.io",
-  user: "u875583157_website",
-  password: "CodeCrafter@032022",
-  database: "u875583157_website",
+  host: process.env.DB_HOST || "127.0.0.1",
+  port: parseInt(process.env.DB_PORT || "3306"),
+  user: process.env.DB_USER || "u875583157_website",
+  password: process.env.DB_PASS || "CodeCrafter@032022",
+  database: process.env.DB_NAME || "u875583157_website",
+  waitForConnections: true,
+  connectionLimit: 10,
+  connectTimeout: 10000,
 });
 
 export default pool;
