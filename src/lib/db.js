f9601512321eff6a103c8b4762acc3bbf27008dc
aarbaj08112000 <162178583+aarbaj08112000@users.noreply.github.com>
2026-06-04 18:26:46 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,3 +1,4 @@
+// lib/db.js
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
@@ -5,8 +6,6 @@ const pool = mysql.createPool({
   user: "u875583157_website",
   password: "CodeCrafter@032022",
   database: "u875583157_website",
-  waitForConnections: true,
-  connectionLimit: 10,
 });
 
 export default pool;
