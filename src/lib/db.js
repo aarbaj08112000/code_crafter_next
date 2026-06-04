523557ad7cc8b091d3dc91fd69ff505e30b0c8b3
aarbaj08112000 <162178583+aarbaj08112000@users.noreply.github.com>
2026-06-04 18:10:14 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "mysql.hostinger.com",
   user: "u875583157_website",
   password: "CodeCrafter@032022",
   database: "u875583157_website",
